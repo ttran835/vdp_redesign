fe17636420536457c46e0e411df2a6d514843f6f
ttran835 <timothy.tran835@gmail.com>
2019-04-30 16:46:30 -0700

    got carousel information working as of this time, will finish all layout before moving into php integration

--- public/components/row_1/img-carousel/img_carousel.js
+++ public/components/row_1/img-carousel/img_carousel.js
@@ -1,31 +1,138 @@
-var imgCarousel = function() {
-  $('#detail-images').slick({
-    asNavFor: '#thumbnail-gallery',
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    fade: true,
-  });
+$('.filter-btn').click(function() {
+  var filterValue = $(this).attr('data-filter');
 
-  var slidesToShow = 10;
-  var slidesAvailable = $('#thumbnail-gallery').children().length - 1;
-  if (slidesAvailable && slidesToShow > slidesAvailable) {
-    slidesToShow = slidesAvailable;
+  $('.filter-btn').removeClass('filter-btn-active');
+  $(this).addClass('filter-btn-active');
+
+  if (filterValue != 'all') {
+    $('.product-showroom-item').hide();
+  } else {
+    $('.product-showroom-item').show();
   }
 
-  $('#thumbnail-gallery').slick({
-    asNavFor: '#detail-images',
-    dots: false,
-    arrows: true,
-    infinite: true,
-    autoplay: true,
+  $("[data-category='" + filterValue + "']").show();
+});
+
+$(document).ready(function() {
+  $('[data-filter=all]').addClass('filter-btn-active');
+});
+
+$('.filter-btn').click(function() {
+  var filterValue = $(this).attr('data-filter');
+
+  $('.filter-btn').removeClass('filter-btn-active');
+  $(this).addClass('filter-btn-active');
+
+  if (filterValue != 'all') {
+    $('.product-showroom-item').animate(
+      {
+        opacity: 0,
+      },
+      200,
+      function() {
+        $('.product-showroom-item').hide();
+        $("[data-category='" + filterValue + "']").show();
+      }
+    );
+  } else {
+    $('.product-showroom-item').animate(
+      {
+        opacity: 0,
+      },
+      200,
+      function() {
+        $('.product-showroom-item').show();
+        $('.product-showroom-item').animate(
+          {
+            opacity: 1,
+          },
+          200,
+          function() {
+            // animation
+          }
+        );
+      }
+    );
+  }
+
+  $("[data-category='" + filterValue + "']").animate(
+    {
+      opacity: 1,
+    },
+    200,
+    function() {
+      //animation complete
+    }
+  );
+});
+
+$(document).ready(function() {
+  $('.product-gallery-detail').slick({
     lazyLoad: 'ondemand',
-    focusOnSelect: true,
-    speed: 300,
-    slidesToShow: slidesToShow,
-    slidesToScroll: 1,
-    adaptiveHeight: true,
+    autoplay: true,
+    infinite: true,
+    asNavFor: '.product-gallery-nav',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          dots: false,
+        },
+      },
+    ],
   });
-};
+  $('.product-gallery-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.product-gallery-detail',
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          dots: true,
+        },
+      },
+    ],
+  });
+});
+
+$(document).ready(function() {
+  $('.specs-tab')
+    .first()
+    .addClass('specs-tab-active');
+  $('.specs-panel-group-list').hide();
+  $('.specs-panel-group-list')
+    .first()
+    .show();
+  $('.specs-panel')
+    .first()
+    .attr('data-panel-status', 'active');
+});
+
+$('.specs-tab').click(function() {
+  var tabFilterValue = $(this).attr('data-tab-filter');
+
+  $('.specs-tab').removeClass('specs-tab-active');
+  $(this).addClass('specs-tab-active');
+  $('.specs-panel').hide();
+  $('.specs-panel').attr('data-panel-status', '');
+  $("[data-panel-category='" + tabFilterValue + "']").show();
+  $("[data-panel-category='" + tabFilterValue + "']").attr(
+    'data-panel-status',
+    'active'
+  );
+});
+
+$('.specs-panel-group').click(function() {
+  $('.specs-panel-group-list').hide();
+  $(this)
+    .find('.specs-panel-group-list')
+    .show();
+});
 
 // module.exports = { imgCarousel };
--- public/index.php
+++ public/index.php
@@ -5,12 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" type="text/css" media="screen" href="styles.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/main.css" />
     <link
       rel="stylesheet"
       type="text/css"
       href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"
     />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css"/>
     <link
       rel="stylesheet"
       href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
@@ -23,6 +24,9 @@
     <div>
       <?php require 'components/inventory-main.php';?>
     </div>
+    <div>
+        <?php require 'components/row_1/img-carousel/img_carousel.php'; ?>
+    </div>
     <script
       src="https://code.jquery.com/jquery-1.12.4.min.js"
       integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
@@ -34,6 +38,7 @@
       src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"
     ></script>
     <script type="text/javascript" src="http://vdp-redesign.com/VDP.js"></script>
+    <script type="text/javascript" src="components/row_1/img-carousel/img_carousel.js"></script>
     <script type="text/javascript" src="components/row_1/img-carousel/img_carousel.js"></script>
   </body>
 </html>
