65fc015ec23ec9f0ec9164df4c542f6c0fc06860
ttran835 <timothy.tran835@gmail.com>
2019-05-03 15:27:43 -0700

    finished with rendering writing css to render standard_specification for screen larger than mobile

--- public/components/row_4/standard_specs/standard_specs.php
+++ public/components/row_4/standard_specs/standard_specs.php
@@ -14,103 +14,203 @@
     <div id="standard-specification-nav-tabs">
     <!-- Nav tabs -->
       <ul id="" class="nav nav-tabs" role="tablist">
-        <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Mechanical</a></li>
+        <li role="presentation"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Mechanical</a></li>
         <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Exterior</a></li>
         <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Entertainment</a></li>
         <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Interior</a></li>
         <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Safety</a></li>
+        <li role="presentation" class="active"><a href="#hide" aria-controls="hide" role="tab" data-toggle="tab">Hide</a></li>
       </ul>
       <!-- Tab panes -->
       <div class="tab-content">
-        <div role="tabpanel" class="tab-pane active" id="home">
+        <div rol="tabpanel" class="tab-pane fade in active" id="hide">
+        </div>
+        <div role="tabpanel" class="tab-pane fade" id="home">
           <ul class="tab-spec-info-list">
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
               Engine: Twin Turbo 6.6L V12 DOHC -inc: variable valve control and direct fuel injection
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
               Transmission: 8-Speed Automatic
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
               Transmission w/Driver Selectable Mode
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             Rear-Wheel Drive
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             2.81 Axle Ratio
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             Engine Oil Cooler
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             90-Amp/Hr Maintenance-Free Battery w/Run Down Protection
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             210 Amp Alternator
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             4-Corner Auto-Leveling Suspension
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             Front And Rear Anti-Roll Bars
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
               Automatic w/Driver Control Height Adjustable Automatic Ride Control Comfort Ride Adaptive 
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             Suspension
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             Hydraulic Power-Assist Speed-Sensing Steering
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             21.8 Gal. Fuel Tank
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             Dual Stainless Steel Exhaust
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             4-Wheel Disc Brakes w/4-Wheel ABS, Front And Rear Vented Discs, Brake Assist, Hill Hold Contro
             </li>
-            
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+              Engine: Twin Turbo 6.6L V12 DOHC -inc: variable valve control and direct fuel injection
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+              Transmission: 8-Speed Automatic
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+              Transmission w/Driver Selectable Mode
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            Rear-Wheel Drive
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            2.81 Axle Ratio
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            Engine Oil Cooler
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            90-Amp/Hr Maintenance-Free Battery w/Run Down Protection
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            210 Amp Alternator
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            4-Corner Auto-Leveling Suspension
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            Front And Rear Anti-Roll Bars
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+              Automatic w/Driver Control Height Adjustable Automatic Ride Control Comfort Ride Adaptive 
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            Suspension
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            Hydraulic Power-Assist Speed-Sensing Steering
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            21.8 Gal. Fuel Tank
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            Dual Stainless Steel Exhaust
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            4-Wheel Disc Brakes w/4-Wheel ABS, Front And Rear Vented Discs, Brake Assist, Hill Hold Contro
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+              Engine: Twin Turbo 6.6L V12 DOHC -inc: variable valve control and direct fuel injection
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+              Transmission: 8-Speed Automatic
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+              Transmission w/Driver Selectable Mode
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            Rear-Wheel Drive
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            2.81 Axle Ratio
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            Engine Oil Cooler
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            90-Amp/Hr Maintenance-Free Battery w/Run Down Protection
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            210 Amp Alternator
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            4-Corner Auto-Leveling Suspension
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            Front And Rear Anti-Roll Bars
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+              Automatic w/Driver Control Height Adjustable Automatic Ride Control Comfort Ride Adaptive 
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            Suspension
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            Hydraulic Power-Assist Speed-Sensing Steering
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            21.8 Gal. Fuel Tank
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            Dual Stainless Steel Exhaust
+            </li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            4-Wheel Disc Brakes w/4-Wheel ABS, Front And Rear Vented Discs, Brake Assist, Hill Hold Contro
+            </li>
             <br />
           </ul>
         </div>
-        <div role="tabpanel" class="tab-pane" id="profile"><ul class="spec-info-list">
-            <li>
+        <div role="tabpanel" class="tab-pane fade" id="profile">
+          <ul class="tab-spec-info-list">
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
               Engine: Twin Turbo 6.6L V12 DOHC -inc: variable valve control and direct fuel injection
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
               Transmission: 8-Speed Automatic
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
               Transmission w/Driver Selectable Mode
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             Rear-Wheel Drive
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             2.81 Axle Ratio
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             Engine Oil Cooler
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             90-Amp/Hr Maintenance-Free Battery w/Run Down Protection
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             210 Amp Alternator
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             4-Corner Auto-Leveling Suspension
             </li>
-            <li>
+            <li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
             Front And Rear Anti-Roll Bars
             </li>
             <br />
-          </ul></div>
-        <div role="tabpanel" class="tab-pane" id="messages">...</div>
-        <div role="tabpanel" class="tab-pane" id="settings">...</div>
+          </ul>
+        </div>
+        <div role="tabpanel" class="tab-pane fade" id="messages">...</div>
+        <div role="tabpanel" class="tab-pane fade" id="settings">...</div>
       </div>
     </div>
     <div class="panel-group" id="spec-accordion">
