e763e61f80ad927f637d4521c63526fcf995ce42
ttran835 <timothy.tran835@gmail.com>
2019-05-06 12:49:43 -0700

    finished php integration, will xfer file over to rolls-royce local for testing

--- public/components/row_5/additional_features/additional_features.php
+++ public/components/row_5/additional_features/additional_features.php
@@ -2,133 +2,20 @@
   <div class="col-xs-12">
     <h3>
       <?php
-        $tokens = 'installed_options';
-        if($tokens) {
-          echo $tokens;
+        if($tokens['installed_options']) {
+          echo $tokens['installed_options'];
         } else {
-          echo 'installed_options';
+          echo 'Installed Options';
         }
       ?>
     </h3>
     <div id="installed-options">
       <ul>
-        <li>
-          Engine: Twin Turbo 6.6L V12 DOHC -inc: variable valve control and direct fuel injection
-        </li>
-        <li>
-          Transmission: 8-Speed Automatic
-        </li>
-        <li>
-          Transmission w/Driver Selectable Mode
-        </li>
-        <li>
-        Rear-Wheel Drive
-        </li>
-        <li>
-        2.81 Axle Ratio
-        </li>
-        <li>
-        Engine Oil Cooler
-        </li>
-        <li>
-        90-Amp/Hr Maintenance-Free Battery w/Run Down Protection
-        </li>
-        <li>
-        210 Amp Alternator
-        </li>
-        <li>
-        4-Corner Auto-Leveling Suspension
-        </li>
-        <li>
-        Front And Rear Anti-Roll Bars
-        </li>
-        <li>
-          Automatic w/Driver Control Height Adjustable Automatic Ride Control Comfort Ride Adaptive 
-        </li>
-        <li>
-        Suspension
-        </li>
-        <li>
-        Hydraulic Power-Assist Speed-Sensing Steering
-        </li>
-        <li>
-          Engine: Twin Turbo 6.6L V12 DOHC -inc: variable valve control and direct fuel injection
-        </li>
-        <li>
-          Transmission: 8-Speed Automatic
-        </li>
-        <li>
-          Transmission w/Driver Selectable Mode
-        </li>
-        <li>
-        Rear-Wheel Drive
-        </li>
-        <li>
-        2.81 Axle Ratio
-        </li>
-        <li>
-        Engine Oil Cooler
-        </li>
-        <li>
-        90-Amp/Hr Maintenance-Free Battery w/Run Down Protection
-        </li>
-        <li>
-        210 Amp Alternator
-        </li>
-        <li>
-        4-Corner Auto-Leveling Suspension
-        </li>
-        <li>
-        Front And Rear Anti-Roll Bars
-        </li>
-        <li>
-          Automatic w/Driver Control Height Adjustable Automatic Ride Control Comfort Ride Adaptive 
-        </li>
-        <li>
-        Suspension
-        </li>
-        <li>
-        Hydraulic Power-Assist Speed-Sensing Steering
-        </li>
-        <li>
-          Engine: Twin Turbo 6.6L V12 DOHC -inc: variable valve control and direct fuel injection
-        </li>
-        <li>
-          Transmission: 8-Speed Automatic
-        </li>
-        <li>
-          Transmission w/Driver Selectable Mode
-        </li>
-        <li>
-        Rear-Wheel Drive
-        </li>
-        <li>
-        2.81 Axle Ratio
-        </li>
-        <li>
-        Engine Oil Cooler
-        </li>
-        <li>
-        90-Amp/Hr Maintenance-Free Battery w/Run Down Protection
-        </li>
-        <li>
-        210 Amp Alternator
-        </li>
-        <li>
-        4-Corner Auto-Leveling Suspension
-        </li>
-        <li>
-        Front And Rear Anti-Roll Bars
-        </li>
-        <li>
-          Automatic w/Driver Control Height Adjustable Automatic Ride Control Comfort Ride Adaptive 
-        </li>
-        <li>
-        Suspension
-        </li>
-        <li>
-        Hydraulic Power-Assist Speed-Sensing Steering
-        </li>
+        <?php 
+          foreach ($vehicle->installedOptions as $standardOtions) {
+            echo '<li>'.$standardOtions.'</li>';
+          }
+        ?>
       </ul>
     </div>
     <div class='vdp-cta'>
