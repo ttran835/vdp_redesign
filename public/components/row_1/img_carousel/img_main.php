aacdf64094c8aba9cde37d8ee9a06736bda094a8
ttran835 <timothy.tran835@gmail.com>
2019-05-29 18:31:37 -0700

    updating css to accomodate for carousel zoom slider

--- public/components/row_1/img_carousel/img_main.php
+++ public/components/row_1/img_carousel/img_main.php
@@ -1,4 +1,9 @@
 <div id="product-gallery-slideshow" class="product-gallery-slideshow" >
+  <div class="zoom-container">
+    <a href="#" class="zoom-init">
+      <i class="fa fa-search"></i>
+    </a>
+  </div>
   <div id="product-gallery-detail">
     <?php 
     // all obj var comes from inventory-main
@@ -10,15 +15,15 @@
        $flip = $image->flip;
        if ($rotate === 90 || $rotate === 270) {
          if ($flip === 0) { 
-           echo "<div><img class='img-responsive' src='{$image->src}' alt='{$vehicle->label}' style='transform: rotate({$rotate}deg) scale(0.6)'></div>";
+           echo "<div><img class='img-responsive zoom-init' src='{$image->src}' alt='{$vehicle->label}' style='transform: rotate({$rotate}deg) scale(0.6)'></div>";
          } else {
-           echo "<div><img class='img-responsive' img src='{$image->src}' alt='{$vehicle->label}' style='transform: rotate({$rotate}deg) scale(0.6) scaleY(-1)'></div>";
+           echo "<div><img class='img-responsive zoom-init' src='{$image->src}' alt='{$vehicle->label}' style='transform: rotate({$rotate}deg) scale(0.6) scaleY(-1)'></div>";
          }
        } else {
          if ($flip === 0) {
-           echo "<div><img class='img-responsive' img src='{$image->src}' alt='{$vehicle->label}' style='transform: rotate({$rotate}deg) scale(1)'></div>";
+           echo "<div><img class='img-responsive zoom-init' src='{$image->src}' alt='{$vehicle->label}' style='transform: rotate({$rotate}deg) scale(1)'></div>";
          } else {
-           echo"<div><img class='img-respsonsive' img src='{$image->src}' alt='{$vehicle->label}' style='transform: rotate({$rotate}deg) scale(1) scaleX(-1)'></div>";
+           echo"<div><img class='img-responsive zoom-init' src='{$image->src}' alt='{$vehicle->label}' style='transform: rotate({$rotate}deg) scale(1) scaleX(-1)'></div>";
          }
        }
      }
@@ -45,6 +50,30 @@
           }
         }
       }
+    ?>
+  </div>
+  <div id="zoom-slider">
+  <?php 
+    // all obj var comes from inventory-main
+    // to rotate images into the correct position    
+    // also render single image for carousel
+     foreach ($vehicle->media->image as $image) {
+       $rotate = $image->rotate;
+       $flip = $image->flip;
+       if ($rotate === 90 || $rotate === 270) {
+         if ($flip === 0) { 
+           echo "<div><img class='img-responsive' src='{$image->src}' alt='{$vehicle->label}' style='transform: rotate({$rotate}deg) scale(0.6)'></div>";
+         } else {
+           echo "<div><img class='img-responsive' img src='{$image->src}' alt='{$vehicle->label}' style='transform: rotate({$rotate}deg) scale(0.6) scaleY(-1)'></div>";
+         }
+       } else {
+         if ($flip === 0) {
+           echo "<div><img class='img-responsive' img src='{$image->src}' alt='{$vehicle->label}' style='transform: rotate({$rotate}deg) scale(1)'></div>";
+         } else {
+           echo"<div><img class='img-respsonsive' img src='{$image->src}' alt='{$vehicle->label}' style='transform: rotate({$rotate}deg) scale(1) scaleX(-1)'></div>";
+         }
+       }
+     }
     ?>
   </div>
 </div>
